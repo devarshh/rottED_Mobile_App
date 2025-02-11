e37129e880e2a531dc81acf7ea36fa3ab14d2f88
devarshh <107152704+devarshh@users.noreply.github.com>
2025-02-11 14:56:30 -0500

    Update MainActivity.kt

--- rottEDD/rottEDD/app/src/main/java/com/example/rottED/MainActivity.kt
+++ rottEDD/rottEDD/app/src/main/java/com/example/rottED/MainActivity.kt
@@ -6,9 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+//import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.rottED.ui.screens.LandingScreen
 import com.example.rottED.ui.screens.ReelsScreen
 import com.example.rottED.ui.theme.rottEDTheme
 
@@ -18,11 +24,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             rottEDTheme {
+                val navController: NavHostController = rememberNavController()
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    Surface(modifier = Modifier.padding(innerPadding)) {
-                        ReelsScreen(this) // ✅ Loads Reels screen correctly
+                    NavHost(
+                        navController = navController,
+                        startDestination = "landing",
+                        modifier = Modifier.padding(innerPadding)
+                    ){
+                        composable("landing") { LandingScreen(navController) }
+                        composable("reels") { ReelsScreen(this@MainActivity) }
                     }
                 }
             }
